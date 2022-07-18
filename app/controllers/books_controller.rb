dfaa07e4aa477a4d6e101eb047164935c84f4f01
43962 <qianqinggongben@gmail.com>
2022-07-18 01:54:50 +0000

    [ADD]4-25

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,4 +2,20 @@ class BooksController < ApplicationController
   def new
     @book = Book.new
   end
+  
+  def show
+    @book = Book.find(params[:id])
+  end
+  
+  def index
+    @books = Book.all
+  end
+  
+  def create
+    @book = Book.new(books_params)
+    @book.user_id = current_user.id
+    @book.save
+    redirect_to books_path
+  end
+  
 end
